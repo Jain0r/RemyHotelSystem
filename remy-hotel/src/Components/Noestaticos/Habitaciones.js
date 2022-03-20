7cd9b43288a17495ff41af314e40e7b4695270f1
Sebas C <sebastian.cano@unmsm.edu.pe>
2022-03-19 20:35:45 -0500

    alexis zz

--- remy-hotel/src/Components/Noestaticos/Habitaciones.js
+++ remy-hotel/src/Components/Noestaticos/Habitaciones.js
@@ -16,9 +16,24 @@ const  Habitaciones = () => {
          Datahabi && Datahabi.map( data => {
            return(
            <div className='images_container' key={data.id}>
+             <div className='hab_overlay_container'>
+            <div className='title_hab_overlay_container'>
              <h4>{data.section}</h4>
+             </div>
+             <div className='info_hab_overlay_container'>
              <p>{data.sub_section}</p>
+             </div>
+             <div>
+             <strong>Precio por noche: </strong>
+             <span>{data.price}</span>
+             <form action=''>
+             <button type='submit'  className='hab_reservar_button'>Reservar</button>
+             </form>
+             </div>
+             </div>
+             <div className='image_overlay_container'>
              <img  className='images_hab' src={`${data.image}`}></img>
+             </div>
            </div>
            )
          })
